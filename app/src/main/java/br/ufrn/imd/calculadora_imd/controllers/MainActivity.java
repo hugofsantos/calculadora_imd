18882f41d4c1f04cc05cc722b5eec39781353c8f
Hugo Santos <hf.santos2332@gmail.com>
2023-10-01 22:21:08 -0300

    Commit inicial

---
+++ app/src/main/java/br/ufrn/imd/calculadora_imd/controllers/MainActivity.java
@@ -0,0 +1,16 @@
+package br.ufrn.imd.calculadora_imd.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.ufrn.imd.calculadora_imd.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
